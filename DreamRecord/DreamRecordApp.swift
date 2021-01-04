570e804af09fcb544257b7081a0b77e03c8b07ce
Kalung Tsang <Tsangkalung@hotmail.com>
2021-01-04 15:28:19 +0800

    Initial Commit

---
+++ DreamRecord/DreamRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamRecordApp.swift
+//  DreamRecord
+//
+//  Created by tsangkalung on 2021/1/4.
+//
+
+import SwiftUI
+
+@main
+struct DreamRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
